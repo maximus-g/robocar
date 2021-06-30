f1807c0e163c5e875f05fbc3db62ebb0691704fe
Xolani Gumbi <xolanigumbi19@gmail.com>
2021-06-30 19:18:11 +0200

    Removed unnecessary comments

--- car/car.ino
+++ car/car.ino
@@ -5,39 +5,43 @@
 #define led 13
 #include <Servo.h>
 
+
+// Car wheels motors
 Servo lServo;
 Servo rServo;
 
-// Sensor
+// Sensor 1
 const int trigPin1 = 22;
 const int echoPin1 = 24;
+// Sensor 2
 const int trigPin2 = 12;
 const int echoPin2 = 9;
-int distance1; // distance of the sensor
+
+int distance1;                                  // sensed distance between the car and an obstacle
 long duration1;
-int distance2; // distance of the sensor
+int distance2;                                  // sensed distance between the car and an obstacle
 long duration2;
 
-RF24 radio(7, 8); // CE, CSN
+RF24 radio(7, 8);                               // CE, CSN
 
-const byte addresses[][6] = {"00001", "00002"};
-// const byte address[6] = "00001";
+
+const byte addresses[][6] = {"00001", "00002"}; // Send/receive addresses
 boolean state = 0;
 int sensor_state1 = 0;
 int sensor_state2 = 0;
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   radio.begin();
-  radio.openWritingPipe(addresses[0]); // 00001
-  radio.openReadingPipe(1, addresses[1]); // 00002
-  // radio.openReadingPipe(0, address);
+  radio.openWritingPipe(addresses[0]);          // 00001
+  radio.openReadingPipe(1, addresses[1]);       // 00002
   radio.setPALevel(RF24_PA_MIN);
-  // radio.startListening();
 
   // car wheels
   lServo.attach(11);
   rServo.attach(10);
+  
   // LEDs
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
@@ -47,17 +51,14 @@ void setup() {
   pinMode(echoPin1, INPUT);
   pinMode(trigPin2, OUTPUT);
   pinMode(echoPin2, INPUT);
-  
 }
 
 void loop() {
   
-  
-  //delay(5);
   radio.startListening();
   
-  if (radio.available()) {
-    
+  if (radio.available())
+  {
     const int text;
     radio.read(&text, sizeof(text));
 
@@ -75,22 +76,18 @@ void loop() {
       if (distance1 > 10)
         {
           digitalWrite(2, HIGH);
-          //delay(500);
           lServo.write(50);
           rServo.write(50);
-          //Serial.println(text);
-          //Serial.println();
-          //Serial.println(distance);
         }
       else
         {
           digitalWrite(2, LOW);
-          //delay(500);
           lServo.write(90);
           rServo.write(90);
           sensor_state1 = 1;
         }
-    } 
+    }
+
     // the car moves backward
     else if (text == 3)
     {
@@ -104,42 +101,34 @@ void loop() {
       if (distance2 > 10)
         {
           digitalWrite(3, HIGH);
-          //delay(500);
           lServo.write(180);
           rServo.write(180);
-          //Serial.println(text);
-          //Serial.println();
-          //Serial.println(distance);
         }
       else
         {
           digitalWrite(3, LOW);
-          //delay(500);
           lServo.write(90);
           rServo.write(90);
           sensor_state2 = 1;
        }
     }
+
     // the car moves right
     else if (text == 4)
     {
       digitalWrite(4, HIGH);
-      //Serial.println(text);
-      //Serial.println();
-      //Serial.println(distance);
       lServo.write(75);
       rServo.write(105);
     }
+
     // the car moves left
      else if (text == 5)
     {
       digitalWrite(5, HIGH);
-      //Serial.println(text);
-      //Serial.println();
-      //Serial.println(distance);
       lServo.write(105);
       rServo.write(75);
     }
+
     // the car stops
     else
     {
@@ -147,30 +136,22 @@ void loop() {
       digitalWrite(3, LOW);
       digitalWrite(4, LOW);
       digitalWrite(5, LOW);
-      //Serial.println(text);
-      //Serial.println();
-      //Serial.println(distance);
       lServo.write(90);
       rServo.write(90);
-      //delay(500);
     }
 
     delay(5);
     radio.stopListening();
-    //Serial.println(distance);
     if(sensor_state1)
     {
       radio.write(&sensor_state1, sizeof(sensor_state1));
-      //Serial.println(sensor_state1);
     }
     else
     {
       radio.write(&sensor_state2, sizeof(sensor_state2));
-      //Serial.println(sensor_state2);
     }
     
     sensor_state1 = 0;
     sensor_state2 = 0;
-    
   }
 }
--- remote/remote.ino
+++ remote/remote.ino
@@ -2,43 +2,46 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-#include <LiquidCrystal.h> // includes the LiquidCrystal Library
+#include <LiquidCrystal.h>                        // includes the LiquidCrystal Library
 
 LiquidCrystal lcd(31, 32, 34, 35, 36, 37);
 
 
 
-RF24 radio(7, 8); // CE, CSN
+RF24 radio(7, 8);                                 // CE, CSN
 
-const int buzzer = 13; //buzzer to arduino pin 9
+const int buzzer = 13;                            //buzzer to arduino pin 9
 const byte addresses[][6] = {"00001", "00002"};
-const int forward = 2; // forward direction
-const int backward = 3; // backward direction
-const int right = 4; // right direction
-const int left = 5; // left direction
-boolean state = 0; // direction
-int sensor_state1 = 0; // led state
+const int forward = 2;                            // forward direction
+const int backward = 3;                           // backward direction
+const int right = 4;                              // right direction
+const int left = 5;                               // left direction
+boolean state = 0;                                // direction
+int sensor_state1 = 0;                            // led state
 
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   radio.begin();
   lcd.begin(16,2);
-  radio.openWritingPipe(addresses[1]); // 00002
-  radio.openReadingPipe(1, addresses[0]); // 00001
+  radio.openWritingPipe(addresses[1]);            // 00002
+  radio.openReadingPipe(1, addresses[0]);         // 00001
   radio.setPALevel(RF24_PA_MIN);
-  pinMode(2, INPUT); // forward pb
-  pinMode(3, INPUT); // backward pb
-  pinMode(4, INPUT); // right pb
-  pinMode(5, INPUT); // left pb
+  pinMode(2, INPUT);                              // forward pb
+  pinMode(3, INPUT);                              // backward pb
+  pinMode(4, INPUT);                              // right pb
+  pinMode(5, INPUT);                              // left pb
   pinMode(buzzer, OUTPUT);
 }
 
 void loop() {
 
   delay(5);
-  
+
   radio.stopListening();
+
+
   // move the car forward
   if(digitalRead(forward) == HIGH)
   {
@@ -50,6 +53,7 @@ void loop() {
     const int text = 2;
     radio.write(&text, sizeof(text));
   }
+
   // move the car backward
   else if(digitalRead(backward) == HIGH)
   {
@@ -61,7 +65,8 @@ void loop() {
     const int text = 3;
     radio.write(&text, sizeof(text));
   }
-  // move the car right
+
+  // move the car to the right
   else if(digitalRead(right) == HIGH)
   {
     lcd.clear();
@@ -72,7 +77,8 @@ void loop() {
     const int text = 4;
     radio.write(&text, sizeof(text));
   }
-  // The car moves left
+
+  // move the car to the left
   else if(digitalRead(left) == HIGH)
   {
     lcd.clear();
@@ -83,6 +89,7 @@ void loop() {
     const int text = 5;
     radio.write(&text, sizeof(text));
   }
+
   // stop the car
   else
   {
@@ -94,18 +101,17 @@ void loop() {
   }
   
 
-  radio.startListening();                   //This sets the module as receiver
+  radio.startListening();                             // This sets the module as receiver
   while(!radio.available());
-  //Looking for incoming data
-  radio.read(&sensor_state1, sizeof(sensor_state1)); //Reading the data
+  radio.read(&sensor_state1, sizeof(sensor_state1));  // Reading the data
   Serial.println(sensor_state1);
   
   if (sensor_state1 == 1)
   {
-    tone(buzzer, 1000); // Send 1KHz sound signal...
-    delay(70);        // ...for 1 sec
-    noTone(buzzer);     // Stop sound...
-    delay(70);        // ...for 1sec
+    tone(buzzer, 1000);                               // Send 1KHz sound signal...
+    delay(70);
+    noTone(buzzer);                                   // Stop sound...
+    delay(70);     
   }
   else
   {
