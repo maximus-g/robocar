cefe5fe29795afbc4c99efd3dc4d5428060c48c5
Xolani Gumbi <xolanigumbi19@gmail.com>
2021-07-03 23:43:08 +0200

    Added comments

--- car/car.ino
+++ car/car.ino
@@ -1,156 +1,157 @@
 
+/* RF module libraries */
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-#define led 13
+/* Servo Motor library */
 #include <Servo.h>
 
+#define led 13
 
-// Car wheels motors
+/* Declaring servos */
 Servo lServo;
 Servo rServo;
 
-// Sensor 1
-const int trigPin1 = 22;
-const int echoPin1 = 24;
-// Sensor 2
-const int trigPin2 = 12;
-const int echoPin2 = 9;
+/* Create the NRF24 object, CE and CSN pins to set up the module to be on standby or active mode */
+RF24 radio(7, 8);
 
-int distance1;                                  // sensed distance between the car and an obstacle
-long duration1;
-int distance2;                                  // sensed distance between the car and an obstacle
-long duration2;
+/* Declaring the rest of the pins */
+const byte addresses[][6] = {"00001", "00002"};
 
-RF24 radio(7, 8);                               // CE, CSN
+/* Delcaring ultrasonic sensor variable pins */
+const int trigPin1 = 22;                      // Trig pin for the front-sensor
+const int echoPin1 = 24;                      // Echo pin for the front-sensor
+const int trigPin2 = 12;                      // Trig pin for the back-sensor
+const int echoPin2 = 9;                       // Echo pin for the back-sensor
+int distance1;                                // Distance between the front-sensor and an obstacle
+long duration1;                               // Travel time
+int distance2;                                // Distance between the back-sensor and an obstacle
+long duration2;                               // Travel time
 
 
-const byte addresses[][6] = {"00001", "00002"}; // Send/receive addresses
-boolean state = 0;
 int sensor_state1 = 0;
 int sensor_state2 = 0;
 
-void setup()
-{
-  Serial.begin(9600);
+void setup() {
   radio.begin();
-  radio.openWritingPipe(addresses[0]);          // 00001
-  radio.openReadingPipe(1, addresses[1]);       // 00002
-  radio.setPALevel(RF24_PA_MIN);
+  radio.openWritingPipe(addresses[0]);        // 00001 as the sending address
+  radio.openReadingPipe(1, addresses[1]);     // 00002 as the receiving address
+  radio.setPALevel(RF24_PA_MIN);              
 
-  // car wheels
+  /* Car DC motor gear box pins */
   lServo.attach(11);
   rServo.attach(10);
-  
-  // LEDs
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
+
+  /* Setting up ultrasonic sensors' pins */
   pinMode(trigPin1, OUTPUT);
   pinMode(echoPin1, INPUT);
   pinMode(trigPin2, OUTPUT);
-  pinMode(echoPin2, INPUT);
+  pinMode(echoPin2, INPUT); 
 }
 
 void loop() {
+  delay(5);
+
+
+  /************ RECEIVING MODE *****************/
   
-  radio.startListening();
-  
+  radio.startListening();                       // Sets up the module to be a receiving
+
+  /* Read if there's incoming data */
   if (radio.available())
   {
-    const int text;
-    radio.read(&text, sizeof(text));
+    const int dir;                              // Direction variable
+    radio.read(&dir, sizeof(dir));              // Read incoming direction
 
-    // the car moves forward
-    if (text == 2) 
+    /* The car moves forward */
+    if (dir == 2) 
     {
+      /* Calculate the distance between the car and the obstacle */
       digitalWrite(trigPin1, LOW);
       delayMicroseconds(2);
       digitalWrite(trigPin1, HIGH);
       delayMicroseconds(10);
       digitalWrite(trigPin1, LOW);
       duration1 = pulseIn(echoPin1, HIGH);
-      distance1 = duration1*0.034/2;
-      
+      distance1 = duration1*0.034/2;               // Calculated distance
+
+      /* If the distance between the car and an obstacle is greater 10 cm, the cars moves forward */
       if (distance1 > 10)
         {
-          digitalWrite(2, HIGH);
           lServo.write(50);
           rServo.write(50);
         }
+      /* Otherwise it stops */
       else
         {
-          digitalWrite(2, LOW);
           lServo.write(90);
           rServo.write(90);
-          sensor_state1 = 1;
+          sensor_state1 = 1;                           // This value is going to be sent back to the remote
         }
-    }
-
-    // the car moves backward
-    else if (text == 3)
+    } 
+    /* The car moves backward */
+    else if (dir == 3)
     {
+      /* Calculate the distance between the car and the obstacle */
       digitalWrite(trigPin2, LOW);
       delayMicroseconds(2);
       digitalWrite(trigPin2, HIGH);
       delayMicroseconds(10);
       digitalWrite(trigPin2, LOW);
       duration2 = pulseIn(echoPin2, HIGH);
-      distance2 = duration2*0.034/2;
+      distance2 = duration2*0.034/2;                     // Calculated distance
+      /* If the distance between the car and an obstacle is greater than 10 cm, the cars reverses */
       if (distance2 > 10)
         {
-          digitalWrite(3, HIGH);
           lServo.write(180);
           rServo.write(180);
         }
+     /* Otherwise it stops */
       else
         {
-          digitalWrite(3, LOW);
           lServo.write(90);
           rServo.write(90);
-          sensor_state2 = 1;
+          sensor_state2 = 1;                              // This value is going to be sent back to the remote
        }
     }
-
-    // the car moves right
-    else if (text == 4)
+    /* The car moves right */
+    else if (dir == 4)
     {
-      digitalWrite(4, HIGH);
+
       lServo.write(75);
       rServo.write(105);
     }
-
-    // the car moves left
-     else if (text == 5)
+    /* The car moves left */
+     else if (dir == 5)
     {
-      digitalWrite(5, HIGH);
       lServo.write(105);
       rServo.write(75);
     }
-
-    // the car stops
+    /* The car stops */
     else
     {
-      digitalWrite(2, LOW);
-      digitalWrite(3, LOW);
-      digitalWrite(4, LOW);
-      digitalWrite(5, LOW);
       lServo.write(90);
       rServo.write(90);
     }
 
     delay(5);
-    radio.stopListening();
+
+    /************ RECEIVING MODE *****************/
+    
+    radio.stopListening();                            // Sets up the module to be a sender
+
+    /* If this sensor-state for the front-sensor is 1, send back to the remote */
     if(sensor_state1)
     {
       radio.write(&sensor_state1, sizeof(sensor_state1));
     }
+    /* If this sensor-state for the back-sensor is 1, send back to the remote */
     else
     {
       radio.write(&sensor_state2, sizeof(sensor_state2));
     }
-    
+
+    /* Resets sensor_states */
     sensor_state1 = 0;
     sensor_state2 = 0;
   }
--- remote/remote.ino
+++ remote/remote.ino
@@ -1,117 +1,127 @@
 
+/* RF module libraries */
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-#include <LiquidCrystal.h>                        // includes the LiquidCrystal Library
+/* LCD library */
+#include <LiquidCrystal.h>
 
+
+/* Create the NRF24 object, CE and CSN pins to set up the module to be on standby or active mode */
+RF24 radio(7, 8);
+/* Declaring LCD pins */
 LiquidCrystal lcd(31, 32, 34, 35, 36, 37);
 
-
-
-RF24 radio(7, 8);                                 // CE, CSN
-
-const int buzzer = 13;                            //buzzer to arduino pin 9
+/* Declaring the rest of the pins */
 const byte addresses[][6] = {"00001", "00002"};
-const int forward = 2;                            // forward direction
-const int backward = 3;                           // backward direction
-const int right = 4;                              // right direction
-const int left = 5;                               // left direction
-boolean state = 0;                                // direction
-int sensor_state1 = 0;                            // led state
+
+int forward = 2;                                // forward direction
+int backward = 3;                               // backward direction
+int right = 4;                                  // right direction
+int left = 5;                                   // left direction
+
+const int buzzer = 13;                          // buzzer on pin 13
+
+boolean state = 0;                              
+int sensor_state1 = 0;                          // led state
+int sensor_state2 = 0;                          // led state
 
 
-void setup()
-{
-  Serial.begin(9600);
-  radio.begin();
-  lcd.begin(16,2);
-  radio.openWritingPipe(addresses[1]);            // 00002
-  radio.openReadingPipe(1, addresses[0]);         // 00001
-  radio.setPALevel(RF24_PA_MIN);
-  pinMode(2, INPUT);                              // forward pb
-  pinMode(3, INPUT);                              // backward pb
-  pinMode(4, INPUT);                              // right pb
-  pinMode(5, INPUT);                              // left pb
-  pinMode(buzzer, OUTPUT);
+void setup() {
+  radio.begin();                                // Initialize the RF module
+  lcd.begin(16,2);                              // Initialize the LCD                              
+  radio.openWritingPipe(addresses[1]);          // 00002 as the sending address
+  radio.openReadingPipe(1, addresses[0]);       // 00001 as the receiving address
+  radio.setPALevel(RF24_PA_MIN);                //
+  pinMode(forward, INPUT);                      // forward pb
+  pinMode(backward, INPUT);                     // backward pb
+  pinMode(right, INPUT);                        // right pb
+  pinMode(left, INPUT);                         // left pb
+  pinMode(buzzer, OUTPUT);                      // Setup the buzzer pin as an output
 }
 
 void loop() {
+  delay(5);                                     // Delay for 5 milli-sec
 
-  delay(5);
-
-  radio.stopListening();
-
-
-  // move the car forward
+  
+  /********* SENDING MODE ******************/
+  
+  radio.stopListening();                        // Sets up the module to be a sender
+  /* Move the car forward */
   if(digitalRead(forward) == HIGH)
   {
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Direction:");
-    lcd.setCursor(0,1);
-    lcd.print("Forward!");
-    const int text = 2;
-    radio.write(&text, sizeof(text));
+    lcd.clear();                                // Clear LCD
+    lcd.setCursor(0,0);                         // Set LCD cursor to the col 0 row 0
+    lcd.print("Direction:");                    // Write "Direction:" on the LCD
+    lcd.setCursor(0,1);                         // Set LCD cursor to the col 0 row 1
+    lcd.print("Forward!");                      // Write "Forward:" on the LCD                      
+    const int dir = 2;                          // Set direction to 2 
+    radio.write(&dir, sizeof(dir));             // Send direction to the car using the RF module
   }
-
-  // move the car backward
+  /* Move the car backward */
   else if(digitalRead(backward) == HIGH)
   {
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Direction:");
-    lcd.setCursor(0,1);
-    lcd.print("Backward!");
-    const int text = 3;
-    radio.write(&text, sizeof(text));
+    lcd.clear();                                // Clear LCD
+    lcd.setCursor(0,0);                         // Set LCD cursor to the col 0 row 0
+    lcd.print("Direction:");                    // Write "Direction:" on the LCD
+    lcd.setCursor(0,1);                         // Set LCD cursor to the col 0 row 1                   
+    lcd.print("Backward!");                     // Write "Backward:" on the LCD
+    const int dir = 3;                          // Set direction to 3
+    radio.write(&dir, sizeof(dir));             // Send direction to the car using the RF module
   }
-
-  // move the car to the right
+  /* Move the car right */
   else if(digitalRead(right) == HIGH)
   {
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Direction:");
-    lcd.setCursor(0,1);
-    lcd.print("Right!");
-    const int text = 4;
-    radio.write(&text, sizeof(text));
+    lcd.clear();                                // Clear LCD
+    lcd.setCursor(0,0);                         // Set LCD cursor to the col 0 row 0
+    lcd.print("Direction:");                    // Write "Direction:" on the LCD
+    lcd.setCursor(0,1);                         // Set LCD cursor to the col 0 row 1                   
+    lcd.print("Right!");                        // Write "Right:" on the LCD
+    const int dir = 4;                          // Set direction to 4
+    radio.write(&dir, sizeof(dir));             // Send direction to the car using the RF module
   }
-
-  // move the car to the left
+  /* The car moves left */
   else if(digitalRead(left) == HIGH)
   {
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Direction:");
-    lcd.setCursor(0,1);
-    lcd.print("Left!");
-    const int text = 5;
-    radio.write(&text, sizeof(text));
+    lcd.clear();                                // Clear LCD
+    lcd.setCursor(0,0);                         // Set LCD cursor to the col 0 row 0
+    lcd.print("Direction:");                    // Write "Direction:" on the LCD
+    lcd.setCursor(0,1);                         // Set LCD cursor to the col 0 row 1                   
+    lcd.print("Left!");                         // Write "Left:" on the LCD
+    const int dir = 5;                          // Set direction to 5
+    radio.write(&dir, sizeof(dir));             // Send direction to the car using the RF module
   }
-
-  // stop the car
+  /* Stop the car */
   else
   {
-    lcd.clear();
-    lcd.setCursor(3,0);
-    lcd.print("STATIONARY");
-    const int text = 0;
-    radio.write(&text, sizeof(text));
+    lcd.clear();                                // Clear LCD
+    lcd.setCursor(3,0);                         // Set LCD cursor to the col 3 row 0
+    lcd.print("STATIONARY");                    // Write "STATIONARY" on the LCD
+    const int dir = 0;                          // Set direction to 5
+    radio.write(&dir, sizeof(dir));             // Send 0 to the car using the RF module
   }
-  
 
-  radio.startListening();                             // This sets the module as receiver
-  while(!radio.available());
-  radio.read(&sensor_state1, sizeof(sensor_state1));  // Reading the data
-  Serial.println(sensor_state1);
+  /********* RECEIVING MODE ******************/
+  
+  radio.startListening();                       // Sets up the module to be a receiver
+  while(!radio.available());                    // Read data for as long as there's available incoming data
+  
+  radio.read(&sensor_state1, sizeof(sensor_state1)); // Reading data from the front sensor
+  radio.read(&sensor_state2, sizeof(sensor_state2)); // Reading data from the back sensor
   
   if (sensor_state1 == 1)
   {
-    tone(buzzer, 1000);                               // Send 1KHz sound signal...
+    tone(buzzer, 1000);                         // Send 1KHz sound signal
+    delay(70);                                  // Delay for 70 mS
+    noTone(buzzer);                             // Stop sound
+    delay(70);                                  // Delay for 70 mS
+  }
+  else if (sensor_state2 == 1)
+  {
+    tone(buzzer, 1000);                         // Send 1KHz sound signal
+    delay(70);                                  // Delay for 70 mS
+    noTone(buzzer);                             // Stop sound
     delay(70);
-    noTone(buzzer);                                   // Stop sound...
-    delay(70);     
   }
   else
   {
